439f4c05fe71d2e6204b5ce1581237fd85c5acf0
Abel Borit <abelboritguitton@gmail.com>
2024-01-30 15:03:30 -0500

    [FEAT] @ViewChild & ElementRef

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+/* se puede pasar el código a este maps.module.ts ya que como está en este módulo entonces todo lo referido a este módulo lo podrá usar o pasará por aquí */
+import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+/* al colocar mapboxgl.accessToken nos aparece un error de que no se puede asignar accessToken porque es una propiedad de solo lectura "Cannot assign to 'accessToken' because it is a read-only property." entonces para solucionarlo se colocará (mapboxgl as any).accessToken */
+(mapboxgl as any).accessToken = '<your access token here>';
+
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
@@ -10,7 +15,6 @@ import { MarkersPageComponent } from './pages/markers-page/markers-page.componen
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 
-
 @NgModule({
   declarations: [
     MiniMapComponent,
@@ -19,11 +23,8 @@ import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.
     FullScreenPageComponent,
     MarkersPageComponent,
     PropertiesPageComponent,
-    ZoomRangePageComponent
+    ZoomRangePageComponent,
   ],
-  imports: [
-    CommonModule,
-    MapsRoutingModule
-  ]
+  imports: [CommonModule, MapsRoutingModule],
 })
-export class MapsModule { }
+export class MapsModule {}
--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,8 +1,5 @@
-import { AfterViewInit, Component } from '@angular/core';
-import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
-
-/* al colocar mapboxgl.accessToken nos aparece un error de que no se puede asignar accessToken porque es una propiedad de solo lectura "Cannot assign to 'accessToken' because it is a read-only property." entonces para solucionarlo se colocará (mapboxgl as any).accessToken */
-(mapboxgl as any).accessToken = '<your access token here>';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   templateUrl: './full-screen-page.component.html',
@@ -10,12 +7,33 @@ import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-g
 })
 /* se coloca el "implements AfterViewInit" porque en el full-screen-page.component.html se necesita un espacio para colocar el mapa entonces nuestra primera idea sería colocarlo en el ngOnInit implementando el OnInit pero en este caso necesito esperarme a que el elemento y todos los elementos HTML estén creados y para eso se usará el AfterViewInit para usar su ngAfterViewInit y nos dice que después que la vista ha sido inicializada y ya tenemos el HTML ahí es donde mandamos a llamar al mapa */
 export class FullScreenPageComponent implements AfterViewInit {
+  /* hacer una referencia a un elemento HTML que usa referencia local */
+  @ViewChild('map')
+  /* se coloca con ? ya que en algún punto en el tiempo este divMap aún no está inicializado ya que técnicamente se inicializa bajo referencia del @ViewChild pero al momento que se crea el componente FullScreenPageComponent este divMap aún no está inicializado */
+  public divMap?: ElementRef;
+
   ngAfterViewInit(): void {
-    const map = new mapboxgl.Map({
-      container: 'map', // container ID
+    // console.log(this.divMap);
+
+    /* hacer una validación ya que si el this.divMap?.nativeElement es null entonces va a haber errores y ni si quiera va a compilar nuestra aplicación. Al hacer esta validación entonces ya podemos quitar el ? y dejarlo con this.divMap.nativeElement */
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    const map = new Map({
+      /* no es correcto hacerlo con id ya que si tenemos muchos mapas entonces no vamos a poder hacer referencia por el id ya que ese id estará duplicado y para solucionarlo entonces tomaremos la referencia directamente al elemento HTML que queremos y al hacerlo por referencia local entonces tendrá una referencia por cada elemento HTML que la tenga */
+      // container: 'map', // container ID // FORMA INCORRECTA
+      container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
     });
   }
 }
+
+/* ******************************************************************************************************************* */
+/* Si al usar el ViewChild aparece un warning como el siguiente "........ CommonJS or AMD dependencies can cause optimization bailouts. For more info see: https://angular.io/guide/build#configuring-commonjs-dependencies" */
+/*
+Cuando se usa una dependencia que está empaquetada con CommonJS, puede resultar en aplicaciones más grandes y lentas. A partir de la versión 10, Angular ahora le advierte cuando su compilación incorpora uno de estos paquetes.
+
+Algunas soluciones en este post:
+  - https://bobbyhadz.com/blog/angular-commonjs-or-amd-dependencies-can-cause-optimization-bailouts
+*/
