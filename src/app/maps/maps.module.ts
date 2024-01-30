d863ee1318723ab2da4942042bc4601ccefa5b8b
Abel Borit <abelboritguitton@gmail.com>
2024-01-30 11:15:18 -0500

    [FEAT] maps-layout.component using "ng g component maps/layout/mapsLayout --skip-selector"

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 
 
 @NgModule({
   declarations: [
     MiniMapComponent,
-    SideMenuComponent
+    SideMenuComponent,
+    MapsLayoutComponent
   ],
   imports: [
     CommonModule,
