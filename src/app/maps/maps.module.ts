abd4ea580c0562959c418a2c40aa921efe9dc8f2
Abel Borit <abelboritguitton@gmail.com>
2024-01-30 11:16:54 -0500

    [FEAT] full-screen-page.component using "ng g component maps/pages/fullScreenPage --skip-selector"

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -5,13 +5,15 @@ import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 
 
 @NgModule({
   declarations: [
     MiniMapComponent,
     SideMenuComponent,
-    MapsLayoutComponent
+    MapsLayoutComponent,
+    FullScreenPageComponent
   ],
   imports: [
     CommonModule,
