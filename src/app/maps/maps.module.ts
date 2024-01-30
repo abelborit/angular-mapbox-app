f43f9ff031ab588db3845f637daffa6868413988
Abel Borit <abelboritguitton@gmail.com>
2024-01-30 11:18:47 -0500

    [FEAT] properties-page.component using "ng g component maps/pages/propertiesPage --skip-selector"

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -7,6 +7,7 @@ import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { MarkersPageComponent } from './pages/markers-page/markers-page.componen
     SideMenuComponent,
     MapsLayoutComponent,
     FullScreenPageComponent,
-    MarkersPageComponent
+    MarkersPageComponent,
+    PropertiesPageComponent
   ],
   imports: [
     CommonModule,
