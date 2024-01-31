a3a0f9210bc92032527c30fa06fe2abdda0d2861
Abel Borit <abelboritguitton@gmail.com>
2024-01-31 18:21:26 -0500

    [FEAT] counter-alone.component using "ng g component standalone/components/counterAlone --standalone"

---
+++ src/app/standalone/components/counter-alone/counter-alone.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-counter-alone',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './counter-alone.component.html',
+  styleUrls: ['./counter-alone.component.css']
+})
+export class CounterAloneComponent {
+
+}
